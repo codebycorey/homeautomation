2c9c1265c969fb2cce71e53763897a2040169f20
Corey O'Donnell <rcoreyodonnell@gmail.com>
2017-09-24 09:55:35 -0400

    Added start of hapi

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,29 @@
-console.log("Hello World");
+
+import * as hapi from "hapi";
+
+const server: hapi.Server = new hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: (request: hapi.Request, reply: hapi.ReplyNoContinue) => {
+        return reply('hello world');
+    }
+})
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    if (!server.info || !server.info.uri) {
+        throw new Error("No server uri");
+    }
+    testing();
+    console.log(`Server running at: ${server.info.uri}`);
+});
